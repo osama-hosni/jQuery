75e4f3dbcae2b7d965e80b1ad799c3279f6ba274
Osama Whida <120475364+osama-hosni@users.noreply.github.com>
2023-03-14 11:26:05 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+// Manipulating css using jquery
+
+// $("h1").css("color", "red");
+// $("h1").css("font-size", "5rem");
+// console.log($("h1").css("font-size"));
+// $("h1").addClass("big-title margin-50");
+
+// Manipulating text using jQuery
+
+// $("h1").text("Good Bye");
+// $("button").text("Don't Click ME!");
+// $("button").html("<em>Here It Is</em>");
+// $("a").attr("href", "https://www.yahoo.com");
+// console.log($("a").attr("href
+
+// adding an event listener using jQuery
+
+// $("h1").click(function() {
+//   $("h1").css("color", "purple");
+// });
+
+
+// adding an event listener using vanila js
+
+// for (var i = 0; i < 5; i++) {
+//   document.querySelectorAll("button")[i].addEventListener("click", function() {
+//     document.querySelector("h1").style.color = "purple";
+//   });
+// }
+
+// adding same event listener using jQuery
+
+// $("button").click(function() {
+//   $("h1").css("color", "purple");
+// });
+
+// keydown event listener
+// $("input").keydown(function(event) {
+//   console.log(event.key);
+// });
+
+// key strokes in the whole page
+// $(document).keydown(function(event) {
+//   console.log(event.key);
+// });
+
+// $(document).keydown(function(event) {
+//   $("h1").text(event.key);
+// });
+
+// $("h1").on("mouseover", function() {
+//   $("h1").css("color", "purple");
+// });
+//
